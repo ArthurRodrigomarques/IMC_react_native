fdac804a7d0308926b855477e55ab05a08128b12
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2023-04-15 18:46:34 -0300

    add

--- src/components/form/ResultImc/style.js
+++ src/components/form/ResultImc/style.js
@@ -19,6 +19,23 @@ const styles = StyleSheet.create({
         fontSize:18,
         color:"#ff0043",
         fontWeight:"bold",
+    },
+    boxSharebutton: {
+        width:"100%",
+        alignItens:"center",
+        marginBottom:10,
+    },
+    shared: {
+        backgroundColor:"#1877f2",
+        borderRadius:50,
+        paddingBottom:5,
+        paddingTop:5,
+    },
+    sharedText: {
+        color:"#fff",
+        fontWeight:"bold",
+        paddingHorizontal:30,
+        
     }
 })
 
